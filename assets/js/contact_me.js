94f2b1fb44002b6b1fa6380eddf8c17f043717f1
Alexa Anderson <alexa.anderson@outlook.com>
2021-03-11 12:35:30 -0600

    contact form sles

--- assets/js/contact_me.js
+++ assets/js/contact_me.js
@@ -37,10 +37,8 @@ $(function() {
 		success: function() {
           // Success message
           $('#success').html("<div class='alert alert-success'>");
-          $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-            .append("</button>");
           $('#success > .alert-success')
-            .append("<strong>Your message has been sent. </strong>");
+            .append("<strong>Message sent, " + firstName + ". You'll be contacted from High Ground with the next step in achieving your design goal!</strong>");
           $('#success > .alert-success')
             .append('</div>');
           //clear all fields
@@ -50,9 +48,8 @@ $(function() {
         error: function() {
           // Fail message
           $('#success').html("<div class='alert alert-danger'>");
-          $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-            .append("</button>");
-          $('#success > .alert-danger').append($("<strong>").text("Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!"));
+
+          $('#success > .alert-danger').append($("<strong>").text("Sorry " + firstName + ", it seems that our mail server is not responding. Please contact us directly at support@highground.tech!"));
           $('#success > .alert-danger').append('</div>');
           //clear all fields
           $('#contactForm').trigger("reset");
